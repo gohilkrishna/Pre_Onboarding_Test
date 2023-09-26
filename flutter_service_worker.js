94abedd38724bacbaa16f80c3757d3e36732c1da
Parmar krishna <krishuedu830684@gmail.com>
2023-09-26 10:46:22 +0530

    new build 26/09/2023

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -360,8 +360,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "de990577000f521942472f17e06eac8c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9a4a6adf9e467904a3dff231e2a23c99",
-"/": "9a4a6adf9e467904a3dff231e2a23c99",
+"index.html": "dff8a510281fd07289781baf9086ef41",
+"/": "dff8a510281fd07289781baf9086ef41",
 "main.dart.js": "c31e3f5dc3ec6781de6c9e17a499d33d",
 "manifest.json": "49b44a351ce78b2f8bbad1e307f52b84",
 "version.json": "f1a07cf1d3dd7dfd9666920b47358d5c"};
